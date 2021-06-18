f4038a96f18b31701432a5ee9c40aa71b1403186
barbara <barbara@moochies.com>
2021-06-18 13:11:26 -0300

    =projeto schedule

---
+++ WorkManagerYt/app/src/main/java/com/example/workmanageryt/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.workmanageryt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.work.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        simpleWork()
+
+        myWorkManager()
+    }
+
+    private fun myWorkManager() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(false)
+            .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
+            .setRequiresCharging(false)
+            .setRequiresBatteryNotLow(true)
+            .build()
+
+        val myRequest = PeriodicWorkRequest.Builder(
+            MyWorker::class.java,
+            15,
+            TimeUnit.MINUTES
+        ).setConstraints(constraints)
+            .build()
+
+        //intervalo minimo de 15min
+
+        WorkManager.getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "my_id",
+                ExistingPeriodicWorkPolicy.KEEP,
+                myRequest
+            )
+    }
+
+    private fun simpleWork() {
+
+        val mRequest: WorkRequest = OneTimeWorkRequestBuilder<MyWorker>()
+            .build()
+
+        WorkManager.getInstance(this)
+            .enqueue(mRequest)
+    }
+}
